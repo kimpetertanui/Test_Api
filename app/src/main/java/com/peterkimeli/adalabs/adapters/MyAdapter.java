cb07dcb2e7f8f21d6a93577fe1f8ddb033732d8a
kimpetertanui <kimelipeter526@gmail.com>
2020-06-19 06:13:12 -0700

    updated the adapter

--- app/src/main/java/com/peterkimeli/adalabs/adapters/MyAdapter.java
+++ app/src/main/java/com/peterkimeli/adalabs/adapters/MyAdapter.java
@@ -35,7 +35,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-       View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+       View v= LayoutInflater.from(parent.getContext())
+               .inflate(R.layout.list_item,parent,false);
        return new ViewHolder(v);
 
     }
